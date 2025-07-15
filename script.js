16c9e3289c439ea980a5fef778a42c05bdfd4c19
guimashan <service@guimashan.org.tw>
2025-07-15 14:56:37 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // 請將此替換為你部署的 Google Apps Script API 的 URL。
 // 這個 URL 將用於處理試算表查詢。
-const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbzaCq3HU_9nwm9pE3E1m9cPDFGqw3qV4hhQ1EwBZJGyIrVnLsduiTW0R0NzREz1R7kD/exec'; 
+const APPS_SCRIPT_URL = 'https://script.google.com/macros/library/d/16PyX6iWPhalUg5hO7PrxWHg3XEJ1YmGjfZeJ-bO8yq8vr8gDamRo4gZZ/12'; 
 
 // 取得網頁上的 HTML 元素參考
 const nameInput = document.getElementById('nameInput');
